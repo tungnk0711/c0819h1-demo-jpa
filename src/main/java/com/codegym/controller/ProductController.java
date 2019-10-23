c3977546b816e02107f92f63843a341da20784cd
nguyenkhanhtung <tungnk0711@gmail.com>
2019-10-23 11:11:49 +0700

    cm

---
+++ src/main/java/com/codegym/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.codegym.controller;
+
+import com.codegym.model.Product;
+import com.codegym.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/list")
+    public ModelAndView findAll() {
+
+        List<Product> productList = productService.findAllHaveBusiness();
+
+        ModelAndView modelAndView = new ModelAndView("/product/list");
+        modelAndView.addObject("products", productList);
+
+        return modelAndView;
+    }
+}
---
+++ src/main/java/com/codegym/repository/ProductRepositoryImpl.java
@@ -0,0 +1,27 @@
+package com.codegym.repository;
+
+import com.codegym.model.Product;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Transactional
+public class ProductRepositoryImpl implements ProductRepository{
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public List<Product> findAll() {
+        TypedQuery<Product> query = em.createQuery("select p from Product p", Product.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public void add(Product product) {
+
+    }
+}
